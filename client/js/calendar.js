3077e92747c45f47aba4515e5f4bdffb9ba81bc9
schneiderjacques <jacques.schneider@gmail.com>
2022-10-18 23:05:38 +0200

    Ajout de la vue du mois basé sur des données

--- client/js/calendar.js
+++ client/js/calendar.js
@@ -1,75 +1,180 @@
-
+                //Regarder si le cours peut commencer
+                //Si il commence on calcule sa durée en nombre de cellules
+                //Sinon on ajoute un <tr> <td></td> </tr> vide
 //Créer une fonction qui à un jour donné renvoie le calendrier d'une journée.
-const hours = [8,9,10,11,12,13,14,15,16,17,18];
-const quart = ["00","15","30","45"];
+const hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+const quart = ["00", "15", "30", "45"];
 const mes_rdv = {
-    "rdvs":[   
+    "rdvs": [
         {
-            id:0,
-            heureDebut: "18-01",
-            heureFin: 1666100700,
-            date: '18-10-2022'
+            id: 0,
+            date: "10-18-2022",
+            heureDebut: "10-18-2022 08:00:00",
+            heureFin: "10-18-2022 09:00:00",
+            nom: "Anniversaire de sam",
         },
         {
-            id:1,
-            heureDebut: 1666105200,
-            heureFin: 1666107900,
-            date: '19-10-2022'
-        }
+            id: 1,
+            date: "10-25-2022",
+            heureDebut: "10-18-2022 14:00:00",
+            heureFin: "10-18-2022 18:00:00",
+            nom: "Cours de math",
+        },
+        {
+            id: 2,
+            date: "09-26-2022",
+            heureDebut: "10-19-2022 14:00:00",
+            heureFin: "10-19-2022 15:00:00",
+            nom: "Cours d'anglais",
+        },
+        {
+            id: 3,
+            date: "10-18-2022",
+            heureDebut: "10-18-2022 15:00:00",
+            heureFin: "10-18-2022 16:00:00",
+            nom: "Sortie VTT",
+        },
 
-]
+    ]
 }
+hideMenu()
 
-createCalendarForADay(mes_rdv.rdvs);
-
-
-function createCalendarForADay(rdvs){
-    var currentHour;
-    let table = document.createElement("table");
-    let thead = document.createElement("thead");
-    let tr = document.createElement("tr");
-    let th = document.createElement("th");
-    th.colSpan = 2;
-    th.innerHTML = "Mercredi 18/10";
-    tr.classList.add("tr-head");
-    tr.appendChild(th);
-    thead.appendChild(tr);
-    table.appendChild(thead);
-
-    let tbody = document.createElement("tbody");
-    table.appendChild(tbody);
-
-    for(let i = 0 ; i < hours.length ; i++){
-        currentHour = hours[i] + ":" + quart[0] //Représente l'heure en cours : 8:00 ; 9:00...
-        createTdOfHours(currentHour, tbody)
-
-        for(let j = 0 ; j < quart.length; j++){
-            let tempTr = document.createElement("tr");
-            tempTr.appendChild(document.createElement("td"));
-            tbody.appendChild(tempTr);
-
-
-            for(let z = 0 ; z < rdvs.length ; z++){
-                console.log(new Date(rdvs[z].heureDebut));
-                console.log(new Date(rdvs[z].heureFin));
-            }
-
-
+    function hideMenu(){
+        document.getElementById('menu-selection').style.display = 'none';
+    }
+    function showMenu(){
+        if(document.getElementById('menu-selection').style.display == 'block'){
+            document.getElementById('menu-selection').style.display = 'none';
+            return;
         }
+        document.getElementById('menu-selection').style.display = 'block';
+
     }
 
 
+    printCalendar(getAllDaysFromMonth(new Date().getMonth(), new Date().getFullYear()), new Date().getMonth());
+    document.getElementById("main-title-month").innerHTML = getMonthName(new Date().getMonth()) + " " + new Date().getFullYear();
 
-    document.getElementById('main-container').appendChild(table)
-}
 
-function createTdOfHours(currentHour, tbody){ //créer un td de rowspan 4 montrant les heures et l'ajoute au tbody
+    function getMonthName(month){
+        const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+        return months[month];
+    }
+    function getAllDaysFromMonth(month, year){
+        let days = []; //Tout les jours du mois
+        let firstDays = []; //Les jours du mois précédents qu'on peut afficher
+        const lastDays = []; //Les jours du mois d'après qu'on peu afficher
 
-    let tempTr = document.createElement("tr");
-    let tempTdHours = document.createElement("td");
-    tempTdHours.classList.add("hours");
-    tempTdHours.rowSpan=4;
-    tempTdHours.innerHTML = currentHour;
-    tempTr.appendChild(tempTdHours)
-    tbody.appendChild(tempTr)
-}
+        for(let i = 1; i <= daysInMonth(month, year); i++){
+            let day = new Date(year, month, i);
+            days.push(day);
+        }
+        firstDayOfMonth = new Date(days[0]);
+        lastDayOfMonth = new Date(days[days.length-1]);
+
+        const previousDays = new Date(days[0]).getDay() - 1; //Jours précédents du mois M étudié
+        const nextdays = 42 - (days.length + previousDays); //Jours suivant qu'on peut toujours remplir
+
+        for(let i = previousDays; i > 0; i--){
+            firstDays.push(getPreviousDay(firstDayOfMonth, i));
+        }
+
+        for(let i = 0; i < nextdays; i++){
+            lastDays.push(getNextDay(lastDayOfMonth, i+1));
+        }
+
+        const final = firstDays.concat(days, lastDays);
+        return final;
+    }
+
+    function daysInMonth (month, year) {
+        return new Date(year, month+1, 0).getDate();
+    }
+
+
+    function getPreviousDay(date, nb) {//renvoie le n jour précédent de date
+        const previous = new Date(date.getTime());
+        previous.setDate(date.getDate() - nb);
+        return previous;
+      }
+      function getNextDay(date,nb){ //renvoie le N jour suivant de date
+        const next = new Date(date.getTime());
+        next.setDate(date.getDate() + nb);
+        return next;
+      }
+
+      function isDateEqual(date1, date2){
+        return date1.getTime() == date2.getTime();
+      }
+
+      function searchDateInArray(date, array){
+        var results = [];
+        for(let i = 0; i < array.length; i++){
+            if(isDateEqual(date, new Date(array[i].date))){
+                results.push(array[i]);
+            }
+        }
+        return results;
+      }
+
+
+      function printCalendar(days, month){
+        for(let i = 0; i < days.length; i++){
+            const dt = new Date(days[i]);
+
+            var main_month_container = document.getElementById("main-month-container");
+            var div = document.createElement('div');
+
+
+            if(dt.getMonth() != month){ //Si le jour concerne notre mois
+                div.classList.add("relative", "bg-gray-50", "py-2", "px-3", "text-gray-500");
+            }else{
+                div.classList.add("relative", "bg-white", "py-2", "px-3");
+            }
+
+
+            var time = document.createElement("time");
+            time.setAttribute("datetime",dt.toISOString().split('T')[0])
+            time.innerHTML = dt.getDate();
+
+            if(dt.getDate() == new Date().getDate()){ //Regarde si c'est la date du jour on rajoute une classe
+                time.classList.add("flex","h-6" , "w-6" , "items-center" , "justify-center" , "rounded-full" , "bg-indigo-600" , "font-semibold", "text-white");
+            }
+
+
+
+            div.appendChild(time);
+
+            const rdv = searchDateInArray(dt, mes_rdv.rdvs);
+                        div.appendChild(time);
+            if(rdv.length != 0){
+                var ol = document.createElement("ol");
+                ol.classList.add("mt-2");
+                for(let j = 0; j < rdv.length; j++){
+                    const li = document.createElement("li");
+                    var a = document.createElement("a");
+                    a.classList.add("groupe", "flex");
+                    a.setAttribute("href", "#");
+                    var p = document.createElement("p");
+                    p.classList.add("flex-auto", "truncate", "font-medium", "text-gray-900", "group-hover:text-indigo-600");
+                    p.innerHTML = rdv[j].nom;
+                    var timeTemp = document.createElement("time");
+                    timeTemp.classList.add("ml-3", "hidden", "flex-none", "text-gray-500", "group-hover:text-indigo-600", "xl:block");
+                    timeTemp.setAttribute("datetime", rdv[j].heureDebut);
+                    timeTemp.innerHTML = rdv[j].heureDebut.split(" ")[1].split(":")[0] + ":" + rdv[j].heureDebut.split(" ")[1].split(":")[1];
+                    a.appendChild(p);
+                    a.appendChild(timeTemp);
+                    li.appendChild(a);
+                    ol.appendChild(li);
+                    div.appendChild(ol);
+                }
+
+            }
+
+
+
+
+            main_month_container.appendChild(div);
+
+        }
+      }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*/*.{html,js}",
-  "./html/*/*.{html,js}"],
+  "./html/*/*.{html,js}",
+  './index.html'],
   theme: {
     extend: {},
   },
