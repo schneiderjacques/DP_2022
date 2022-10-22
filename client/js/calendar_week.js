cfd7e4d2f4c8c369214efc7b92e305b965d65c36
schneiderjacques <jacques.schneider19@gmail.com>
2022-10-22 11:56:26 +0200

    Passage d'une semaine à une autre

--- client/js/calendar_week.js
+++ client/js/calendar_week.js
@@ -1,3 +1,5 @@
+import { getPreviousDay, getNextDay } from './date_tools.js'
+
 
 const heures = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
 
@@ -16,18 +18,70 @@ function initHours() {
     }
 }
 
+var currentWeekDate = new Date();
 
-setWeekTableHeaderByDay(new Date());
+
+
+document.getElementById('btn-next-week').addEventListener('click', function () { //Bouton pour afficher le mois suivant
+    currentWeekDate = getNextDay(currentWeekDate, 8);
+    refreshCalendar(currentWeekDate)
+
+})
+
+document.getElementById('btn-previous-week').addEventListener('click', function () { //Bouton pour afficher le mois précédent
+    currentWeekDate = getPreviousDay(currentWeekDate, 6);
+    refreshCalendar(currentWeekDate)
+})
+
+
+
+
+
+
+
+
+setWeekTableHeaderByDay(currentWeekDate);
+
+function refreshCalendar(date){
+    document.getElementById("week-headers").innerHTML = "";
+    var div = document.createElement("div");
+    div.classList.add("col-end-1", "w-14");
+    document.getElementById("week-headers").appendChild(div);
+    document.getElementById("week-headers-responsive").innerHTML = "";
+    setWeekTableHeaderByDay(date);
+}
+
+
+
+
+function setDateName(date){
+    var button = document.getElementById("btn-week-name");
+    button.innerHTML = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+}
+
+function setWeekNumber(dt){
+    var startDate = new Date(dt.getFullYear(), 0, 1);
+    var days = Math.floor((dt - startDate) /
+        (24 * 60 * 60 * 1000));
+         
+    var weekNumber = Math.ceil(days / 7);
+    document.getElementById("main-title-week").innerHTML = "Semaine " + weekNumber;
+}
+
 
 function setWeekTableHeaderByDay(dt){ //Met en place le header du tableau (Lun 10, Mar 11, etc...)
     const firstDayOfWeek = getFirstDayOfWeek(dt);
     const week = getAllDaysFromWeek(firstDayOfWeek);
+    setWeekNumber(dt); //Met en place le numéro de la semaine;
     var parent = document.getElementById("week-headers");
     var parent_responsive = document.getElementById("week-headers-responsive");
     for (let i = 0; i < week.length; i++) {
-        const name = getDayName(week[i]).slice(0,3) + " ";
-        const day = week[i].getDate();
-        var div = document.createElement("div");
+
+        if (i == 0) setDateName(week[i]); //Met en place la date de la semaine
+
+        const name = getDayName(week[i]).slice(0,3) + " "; //récupère le nom du jour et le coupe à 3 lettres
+        const day = week[i].getDate(); //On récupère le jour
+        var div = document.createElement("div"); //on créer une div qui contiendra le nom du jour et le numéro du jour
         div.classList.add("flex", "items-center", "justify-center", "py-3");
         var span = document.createElement("span");
         span.innerHTML = name;
