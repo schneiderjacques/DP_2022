18c932b0d67732a99498a97bbeb5c37ebbbbd97c
schneiderjacques <icrystal.fr@gmail.com>
2022-10-30 19:16:28 +0100

    Correction

--- client/js/request.js
+++ client/js/request.js
@@ -1,10 +1,10 @@
-const api = 'http://localhost:8080/'; // adresse l'api avec le port
+const api = "http://localhost:8080/"; // adresse l'api avec le port
 export async function fetchData(body, method, route) {
   const response = await fetch(api + route, {
     method: method,
     headers: {
-      'Content-Type':'application/json',
-      'Accept':'application/json'
+      "Content-Type":"application/json",
+      "Accept":"application/json",
     },
     body: body
     });   
@@ -14,22 +14,22 @@ export async function fetchDataConnected(body, method, route) {
   const response = await fetch(api + route, {
     method: method,
     headers: {
-      'Content-Type':'application/json',
-      'Accept':'application/json',
-      'Authorization': 'Bearer ' + JSON.parse(window.localStorage.getItem('user')).token,
+      "Content-Type":"application/json",
+      "Accept":"application/json",
+      "Authorization": "Bearer " + JSON.parse(window.localStorage.getItem('user')).token,
     },
     body: body
   });        
   return response.json();
 }
-export async function fetchDataGet(route){
+export async function fetchDataGet(route) {
   const response = await fetch(api + route, {
-    method: 'GET',
-    headers: {
-      'Content-Type':'application/json',
-      'Accept':'application/json',
-      'Authorization': 'Bearer ' + JSON.parse(window.localStorage.getItem('user')).token,
-    }
-  });;
+  method: 'GET',
+  headers: {
+    "Content-Type":"application/json",
+    "Accept":"application/json",
+    "Authorization": "Bearer " + JSON.parse(window.localStorage.getItem('user')).token,
+  },
+  });
 return response.json();
 }
