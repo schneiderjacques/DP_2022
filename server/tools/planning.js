91cdfab5cfc8832abc7728891e88cb1f346ae424
Silvio <silvio.brancati4@etu.univ-lorraine.fr>
2022-11-01 14:45:38 +0100

    [DP-30] Ajout de la vérification des champs

--- server/tools/planning.js
+++ server/tools/planning.js
@@ -1,5 +1,6 @@
 const tool_user = require("./user.js");
 const fs = require("fs");
+const check_regex = require("./regex_checks.js");
 
 /**
  * Méthode permettant de récupérer les données du site
@@ -7,19 +8,19 @@ const fs = require("fs");
  * @returns {[]|[{date: string, heureDebut: string, id: number, heureFin: string, nom: string},{date: string, heureDebut: string, id: number, heureFin: string, nom: string},{date: string, heureDebut: string, id: number, heureFin: string, nom: string},{date: string, heureDebut: string, id: number, heureFin: string, nom: string},{date: string, heureDebut: string, id: number, heureFin: string, nom: string}]|*|null}
  */
 function get_planning(req) {
-  // Récupérer les données du site
-  let users = JSON.parse(
-    fs.readFileSync(__dirname + "/../data/" + "data.json", "utf8")
-  );
+    // Récupérer les données du site
+    let users = JSON.parse(
+        fs.readFileSync(__dirname + "/../data/" + "data.json", "utf8")
+    );
 
-  // Chercher l'utilisateur
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].token == req.headers.authorization.substring(7)) {
-      return users[i].rdvs;
+    // Chercher l'utilisateur
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].token == req.headers.authorization.substring(7)) {
+            return users[i].rdvs;
+        }
     }
-  }
 
-  return null;
+    return null;
 }
 
 /**
@@ -30,36 +31,42 @@ function get_planning(req) {
  * Réponse
  */
 function month_view(req, res) {
-  // Vérifier si le mois et l'année sont présents dans la requête
-  if (!req.params.month || !req.params.year) {
-    res.sendStatus(422);
-    return;
-  }
-
-  // Récupérer le planning de l'utilisateur
-  let planning = get_planning(req);
-
-  if (planning) {
-    // Récupérer les rendez-vous du mois
-    let rdvs = [];
-    for (let i = 0; i < planning.length; i++) {
-      let date = new Date(planning[i].date);
-      if (
-        date.getMonth() + 1 == req.params.month &&
-        date.getFullYear() == req.params.year
-      ) {
-        rdvs.push(planning[i]);
-      }
+    // Vérifier si le mois et l'année sont présents dans la requête
+    if (!req.params.month || !req.params.year) {
+        res.sendStatus(422);
+        return;
     }
-    res.set({
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    });
-    res.send(rdvs);
-  } else {
-    res.sendStatus(404);
-    return;
-  }
+
+    // Vérifier si le mois et l'année sont des nombres
+    if (isNaN(req.params.month) || isNaN(req.params.year) || req.params.month < 1 || req.params.month > 12) {
+        res.sendStatus(422);
+        return;
+    }
+
+    // Récupérer le planning de l'utilisateur
+    let planning = get_planning(req);
+
+    if (planning) {
+        // Récupérer les rendez-vous du mois
+        let rdvs = [];
+        for (let i = 0; i < planning.length; i++) {
+            let date = new Date(planning[i].date);
+            if (
+                date.getMonth() + 1 == req.params.month &&
+                date.getFullYear() == req.params.year
+            ) {
+                rdvs.push(planning[i]);
+            }
+        }
+        res.set({
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        });
+        res.send(rdvs);
+    } else {
+        res.sendStatus(404);
+        return;
+    }
 }
 
 /**
@@ -70,41 +77,47 @@ function month_view(req, res) {
  * Réponse
  */
 function week_view(req, res) {
-  // Vérifier si le mois et l'année sont présents dans la requête
-  if (!req.params.first_day_week) {
-    res.sendStatus(422);
-    return;
-  }
-
-  // Récupérer le planning de l'utilisateur
-  let planning = get_planning(req);
-
-  if (planning) {
-    // Récupérer les rendez-vous de la semaine
-    let rdvs = [];
-
-    // Récupérer la date de début et de fin de la semaine
-    let start_week = new Date(req.params.first_day_week);
-    let end_week = new Date(start_week);
-    end_week.setDate(end_week.getDate() + 6);
-
-    for (var i = 0; i < planning.length; i++) {
-      let date = new Date(planning[i].date);
-
-      // Vérifier si la date du rendez-vous est dans la semaine
-      if (date >= start_week && date <= end_week) {
-        rdvs.push(planning[i]);
-      }
+    // Vérifier si le mois et l'année sont présents dans la requête
+    if (!req.params.first_day_week) {
+        res.sendStatus(422);
+        return;
     }
-    res.set({
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    });
-    res.send(rdvs);
-  } else {
-    res.sendStatus(404);
-    return;
-  }
+
+    // Vérifier le format de la date
+    if (!check_regex.check_date(req.params.first_day_week)) {
+        res.sendStatus(422);
+        return;
+    }
+
+    // Récupérer le planning de l'utilisateur
+    let planning = get_planning(req);
+
+    if (planning) {
+        // Récupérer les rendez-vous de la semaine
+        let rdvs = [];
+
+        // Récupérer la date de début et de fin de la semaine
+        let start_week = new Date(req.params.first_day_week);
+        let end_week = new Date(start_week);
+        end_week.setDate(end_week.getDate() + 6);
+
+        for (var i = 0; i < planning.length; i++) {
+            let date = new Date(planning[i].date);
+
+            // Vérifier si la date du rendez-vous est dans la semaine
+            if (date >= start_week && date <= end_week) {
+                rdvs.push(planning[i]);
+            }
+        }
+        res.set({
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        });
+        res.send(rdvs);
+    } else {
+        res.sendStatus(404);
+        return;
+    }
 }
 
 /**
@@ -115,36 +128,42 @@ function week_view(req, res) {
  * Réponse
  */
 function day_view(req, res) {
-  // Vérifier si le mois et l'année sont présents dans la requête
-  if (!req.params.day) {
-    res.sendStatus(422);
-    return;
-  }
-
-  // Récupérer le planning de l'utilisateur
-  let planning = get_planning(req);
-
-  if (planning) {
-    // Récupérer les rendez-vous de la journée
-    let rdvs = [];
-    let date_matching = new Date(req.params.day);
-    for (var i = 0; i < planning.length; i++) {
-      let date = new Date(planning[i].date);
-
-      // Vérifier si la date du rendez-vous est dans la journée
-      if (date.getDate() === date_matching.getDate()) {
-        rdvs.push(planning[i]);
-      }
+    // Vérifier si le mois et l'année sont présents dans la requête
+    if (!req.params.day) {
+        res.sendStatus(422);
+        return;
     }
-    res.set({
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    });
-    res.send(rdvs);
-  } else {
-    res.sendStatus(404);
-    return;
-  }
+
+    // Vérifier le format de la date
+    if (!check_regex.check_date(req.params.day)) {
+        res.sendStatus(422);
+        return;
+    }
+
+    // Récupérer le planning de l'utilisateur
+    let planning = get_planning(req);
+
+    if (planning) {
+        // Récupérer les rendez-vous de la journée
+        let rdvs = [];
+        let date_matching = new Date(req.params.day);
+        for (var i = 0; i < planning.length; i++) {
+            let date = new Date(planning[i].date);
+
+            // Vérifier si la date du rendez-vous est dans la journée
+            if (date.getDate() === date_matching.getDate()) {
+                rdvs.push(planning[i]);
+            }
+        }
+        res.set({
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        });
+        res.send(rdvs);
+    } else {
+        res.sendStatus(404);
+        return;
+    }
 }
 
 /**
@@ -155,65 +174,71 @@ function day_view(req, res) {
  * Réponse
  */
 function add_appointment(req, res) {
-  // Vérifier si les données sont présentes dans la requête
-  if (
-    !req.body.nom ||
-    !req.body.date ||
-    !req.body.heureDebut ||
-    !req.body.heureFin ||
-    !req.body.couleur
-  ) {
-    res.sendStatus(422);
-    return;
-  }
-
-  // Récupérer le planning de l'utilisateur
-  let planning = get_planning(req);
-
-  if (planning) {
-    // Récupérer les données du site
-    let users = tool_user.get_datas();
-    // Chercher l'utilisateur
-    for (var i = 0; i < users.length; i++) {
-      if (users[i].token == req.headers.authorization.substring(7)) {
-        let new_date_debut = new Date(req.body.heureDebut);
-        let new_date_fin = new Date(req.body.heureFin);
-
-        // Vérifier si le rendez-vous n'est pas déjà pris
-        for (let j = 0; j < planning.length; j++) {
-          let debut = new Date(planning[j].heureDebut);
-          let fin = new Date(planning[j].heureFin);
-
-          if (debut <= new_date_debut && fin >= new_date_fin) {
-            res.sendStatus(409);
-            return;
-          }
-        }
-
-        // Ajouter le rendez-vous
-        users[i].rdvs.push({
-          id: planning.length + 1,
-          nom: req.body.nom,
-          date: req.body.date,
-          heureDebut: req.body.heureDebut,
-          heureFin: req.body.heureFin,
-          couleur: req.body.couleur,
-        });
-
-        // Sauvegarder les données
-        fs.writeFileSync(
-          __dirname + "/../data/" + "data.json",
-          JSON.stringify(users)
-        );
-
-        res.sendStatus(200);
+    // Vérifier si les données sont présentes dans la requête
+    if (
+        !req.body.nom ||
+        !req.body.date ||
+        !req.body.heureDebut ||
+        !req.body.heureFin ||
+        !req.body.couleur
+    ) {
+        res.sendStatus(422);
         return;
-      }
     }
-  } else {
-    res.sendStatus(404);
-    return;
-  }
+
+    // Vérifier la validité des données
+    if (!check_regex.check_appointment(req.body)) {
+        res.sendStatus(422);
+        return;
+    }
+
+    // Récupérer le planning de l'utilisateur
+    let planning = get_planning(req);
+
+    if (planning) {
+        // Récupérer les données du site
+        let users = tool_user.get_datas();
+        // Chercher l'utilisateur
+        for (var i = 0; i < users.length; i++) {
+            if (users[i].token == req.headers.authorization.substring(7)) {
+                let new_date_debut = new Date(req.body.heureDebut);
+                let new_date_fin = new Date(req.body.heureFin);
+
+                // Vérifier si le rendez-vous n'est pas déjà pris
+                for (let j = 0; j < planning.length; j++) {
+                    let debut = new Date(planning[j].heureDebut);
+                    let fin = new Date(planning[j].heureFin);
+
+                    if (debut <= new_date_debut && fin >= new_date_fin) {
+                        res.sendStatus(409);
+                        return;
+                    }
+                }
+
+                // Ajouter le rendez-vous
+                users[i].rdvs.push({
+                    id: planning.length + 1,
+                    nom: req.body.nom,
+                    date: req.body.date,
+                    heureDebut: req.body.heureDebut,
+                    heureFin: req.body.heureFin,
+                    couleur: req.body.couleur,
+                });
+
+                // Sauvegarder les données
+                fs.writeFileSync(
+                    __dirname + "/../data/" + "data.json",
+                    JSON.stringify(users)
+                );
+
+                res.sendStatus(200);
+                return;
+            }
+        }
+    } else {
+        res.sendStatus(404);
+        return;
+    }
 }
 
 /**
@@ -224,70 +249,78 @@ function add_appointment(req, res) {
  * Réponse
  */
 function edit_appointment(req, res) {
-  // Vérifier si les données sont présentes dans la requête
-  if (
-    !req.body.nom ||
-    !req.body.date ||
-    !req.body.heureDebut ||
-    !req.body.heureFin
-  ) {
-    res.sendStatus(422);
-    return;
-  }
-
-  // Récupérer les données du site
-  let users = tool_user.get_datas();
-
-  // Chercher l'utilisateur
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].token == req.headers.authorization.substring(7)) {
-      let new_date_debut = new Date(req.body.heureDebut);
-      let new_date_fin = new Date(req.body.heureFin);
-
-      // Vérifier si le rendez-vous n'est pas déjà pris
-      for (let j = 0; j < users[i].rdvs.length; j++) {
-        let debut = new Date(users[i].rdvs.heureDebut);
-        let fin = new Date(users[i].rdvs.heureFin);
-
-        if (debut <= new_date_debut && fin >= new_date_fin) {
-          res.sendStatus(409);
-          return;
-        }
-      }
-
-      // Modifier le rendez-vous
-      let found = false;
-      for (let j = 0; j < users[i].rdvs.length; j++) {
-        if (users[i].rdvs[j].id === req.body.id) {
-          found = true;
-          users[i].rdvs[j].nom = req.body.nom;
-          users[i].rdvs[j].date = req.body.date;
-          users[i].rdvs[j].heureDebut = req.body.heureDebut;
-          users[i].rdvs[j].heureFin = req.body.heureFin;
-        }
-      }
-
-      if (!found) {
-        res.sendStatus(404);
+    // Vérifier si les données sont présentes dans la requête
+    if (
+        !req.body.nom ||
+        !req.body.date ||
+        !req.body.heureDebut ||
+        !req.body.heureFin ||
+        !req.body.couleur
+    ) {
+        res.sendStatus(422);
         return;
-      }
-
-      // Sauvegarder les données
-      fs.writeFileSync(
-        __dirname + "/../data/" + "data.json",
-        JSON.stringify(users)
-      );
-
-      res.sendStatus(200);
-      return;
     }
-  }
+
+    // Vérifier la validité des données
+    if (!check_regex.check_appointment(req.body)) {
+        res.sendStatus(422);
+        return;
+    }
+
+    // Récupérer les données du site
+    let users = tool_user.get_datas();
+
+    // Chercher l'utilisateur
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].token == req.headers.authorization.substring(7)) {
+            let new_date_debut = new Date(req.body.heureDebut);
+            let new_date_fin = new Date(req.body.heureFin);
+
+            // Vérifier si le rendez-vous n'est pas déjà pris
+            for (let j = 0; j < users[i].rdvs.length; j++) {
+                let debut = new Date(users[i].rdvs.heureDebut);
+                let fin = new Date(users[i].rdvs.heureFin);
+
+                if (debut <= new_date_debut && fin >= new_date_fin) {
+                    res.sendStatus(409);
+                    return;
+                }
+            }
+
+            // Modifier le rendez-vous
+            let found = false;
+            for (let j = 0; j < users[i].rdvs.length; j++) {
+                if (users[i].rdvs[j].id === req.body.id) {
+                    found = true;
+                    users[i].rdvs[j].nom = req.body.nom;
+                    users[i].rdvs[j].date = req.body.date;
+                    users[i].rdvs[j].heureDebut = req.body.heureDebut;
+                    users[i].rdvs[j].heureFin = req.body.heureFin;
+                    users[i].rdvs[j].couleur = req.body.couleur;
+                }
+            }
+
+            if (!found) {
+                res.sendStatus(404);
+                return;
+            }
+
+            // Sauvegarder les données
+            fs.writeFileSync(
+                __dirname + "/../data/" + "data.json",
+                JSON.stringify(users)
+            );
+
+            res.sendStatus(200);
+            return;
+        }
+    }
 }
 
 module.exports = {
-  month_view: month_view,
-  week_view: week_view,
-  day_view: day_view,
-  add_appointment: add_appointment,
-  edit_appointment: edit_appointment,
+    month_view: month_view,
+    week_view: week_view,
+    day_view: day_view,
+    add_appointment: add_appointment,
+    edit_appointment: edit_appointment,
 };
---
+++ server/tools/regex_checks.js
@@ -0,0 +1,57 @@
+/**
+ * Méthode vérifiant le format d'une date
+ * @param date
+ * Date à vérifier
+ * @returns {boolean}
+ */
+function check_date(date) {
+    return /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/.test(date);
+}
+
+/**
+ * Méthode vérifiant le format d'une couleur
+ * @param couleur
+ *  Couleur à vérifier
+ * @returns {boolean}
+ */
+function check_color(couleur) {
+    return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(couleur);
+}
+
+/**
+ * Méthode vérifiant la validité d'un rendez-vous
+ * @param date
+ * Date du rendez-vous
+ * @param heureDebut
+ * Heure de début du rendez-vous
+ * @param heureFin
+ * Heure de fin du rendez-vous
+ * @param couleur
+ * Couleur du rendez-vous
+ * @returns {boolean}
+ */
+function check_appointment(body) {
+    // Vérifier le format de la date
+    if (!check_date(body.date)) {
+        return false;
+    }
+
+    // Vérifier le format de l'heure
+    if (!/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$/.test(body.heureDebut)
+        || !/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$/.test(body.heureFin)) {
+        return false;
+    }
+
+    // Vérifier le format de la couleur
+    if (!check_color(body.couleur)) {
+        return false;
+    }
+
+    return true;
+}
+
+module.exports = {
+    check_date: check_date,
+    check_color: check_color,
+    check_appointment: check_appointment
+}
