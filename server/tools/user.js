2a6e0747662a04b126301279689c61f8489dd049
Silvio <silvio.brancati4@etu.univ-lorraine.fr>
2022-11-01 14:11:57 +0100

    Fix token

--- server/tools/user.js
+++ server/tools/user.js
@@ -5,10 +5,10 @@ const fs = require("fs");
  * @returns {any}
  */
 function get_datas() {
-  // Récupérer les données du site
-  return JSON.parse(
-    fs.readFileSync(__dirname + "/../data/" + "data.json", "utf8")
-  );
+    // Récupérer les données du site
+    return JSON.parse(
+        fs.readFileSync(__dirname + "/../data/" + "data.json", "utf8")
+    );
 }
 
 /**
@@ -19,34 +19,38 @@ function get_datas() {
  *  Réponse
  */
 function login(req, res) {
-  // Vérifier si le username et le password sont présents dans la requête
-  if (!req.body.username || !req.body.password) {
-    res.sendStatus(422);
-    return;
-  }
-
-  // Récupérer les données du site
-  let users = get_datas();
-
-  // Vérifier si le username et le password sont présents dans la base de données
-  for (let i = 0; i < users.length; i++) {
-    if (
-      users[i].username === req.body.username &&
-      users[i].password === req.body.password
-    ) {
-      // Génération d'un token de session
-      users[i].token = Math.floor(Date.now() / 1000) + 60 * 60;
-      res.set({
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      });
-      res.send(users[users.length - 1]);
-      return;
+    // Vérifier si le username et le password sont présents dans la requête
+    if (!req.body.username || !req.body.password) {
+        res.sendStatus(422);
+        return;
     }
-  }
 
-  // Si le username et le password ne sont pas présents dans la base de données
-  res.sendStatus(401);
+    // Récupérer les données du site
+    let users = get_datas();
+
+    // Vérifier si le username et le password sont présents dans la base de données
+    for (let i = 0; i < users.length; i++) {
+        if (
+            users[i].username === req.body.username &&
+            users[i].password === req.body.password
+        ) {
+            // Génération d'un token de session
+            users[i].token = Math.random().toString(36).substr(2);
+
+            // Enregistrer les données du site
+            fs.writeFileSync(__dirname + "/../data/" + "data.json", JSON.stringify(users));
+
+            res.set({
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            });
+            res.send(users[i]);
+            return;
+        }
+    }
+
+    // Si le username et le password ne sont pas présents dans la base de données
+    res.sendStatus(401);
 }
 
 /**
@@ -57,51 +61,51 @@ function login(req, res) {
  *  Réponse
  */
 function register(req, res) {
-  // Vérifier si le username, le password et le password_confirmation sont présents dans la requête
-  if (!req.body.username || !req.body.password || !req.body.confirm_password) {
-    res.sendStatus(422);
-    return;
-  }
-
-  // Vérifier si le password et le password_confirmation sont identiques
-  if (req.body.password !== req.body.confirm_password) {
-    res.sendStatus(422);
-    return;
-  }
-
-  // Récupérer les données du site
-  let users = get_datas();
-
-  // Vérifier si le username est déjà présent dans la base de données
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].username === req.body.username) {
-      res.sendStatus(409);
-      return;
+    // Vérifier si le username, le password et le password_confirmation sont présents dans la requête
+    if (!req.body.username || !req.body.password || !req.body.confirm_password) {
+        res.sendStatus(422);
+        return;
     }
-  }
 
-  // Ajouter l'utilisateur dans la base de données
-  users.push({
-    id: users.length + 1,
-    username: req.body.username,
-    password: req.body.password,
-    // Génération d'un token de session
-    token: Math.floor(Date.now() / 1000) + 60 * 60,
-    rdvs: [],
-  });
+    // Vérifier si le password et le password_confirmation sont identiques
+    if (req.body.password !== req.body.confirm_password) {
+        res.sendStatus(422);
+        return;
+    }
 
-  // Enregistrer les données du site
-  fs.writeFileSync(
-    __dirname + "/../data/" + "data.json",
-    JSON.stringify(users)
-  );
+    // Récupérer les données du site
+    let users = get_datas();
 
-  // On retourne le dernier utilisateur ajouté
-  res.set({
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-  });
-  res.send(users[users.length - 1]);
+    // Vérifier si le username est déjà présent dans la base de données
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].username === req.body.username) {
+            res.sendStatus(409);
+            return;
+        }
+    }
+
+    // Ajouter l'utilisateur dans la base de données
+    users.push({
+        id: users.length + 1,
+        username: req.body.username,
+        password: req.body.password,
+        // Génération d'un token de session
+        token: Math.random().toString(36).substr(2),
+        rdvs: [],
+    });
+
+    // Enregistrer les données du site
+    fs.writeFileSync(
+        __dirname + "/../data/" + "data.json",
+        JSON.stringify(users)
+    );
+
+    // On retourne le dernier utilisateur ajouté
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
+    res.send(users[users.length - 1]);
 }
 
 /**
@@ -112,31 +116,31 @@ function register(req, res) {
  * Réponse
  */
 function get_user(req, res) {
-  // Récupérer les données du site
-  let users = get_datas();
+    // Récupérer les données du site
+    let users = get_datas();
 
-  // Récupérer l'utilisateur
-  let found = false;
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].token == req.headers.authorization.substring(7)) {
-      found = true;
-      res.set({
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      });
-      res.send(users[i]);
-      return;
+    // Récupérer l'utilisateur
+    let found = false;
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].token == req.headers.authorization.substring(7)) {
+            found = true;
+            res.set({
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            });
+            res.send(users[i]);
+            return;
+        }
     }
-  }
 
-  if (!found) {
-    res.sendStatus(404);
-  }
+    if (!found) {
+        res.sendStatus(404);
+    }
 }
 
 module.exports = {
-  login: login,
-  register: register,
-  get_user: get_user,
-  get_datas: get_datas,
+    login: login,
+    register: register,
+    get_user: get_user,
+    get_datas: get_datas,
 };
